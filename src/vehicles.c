43e8972d1a9dfe072b57b430a59d7f6c4b099fbd
DiptarshiC <dich4615@colorado.edu>
2019-10-28 13:22:53 -0600

    started writing the vehicles.c file

--- src/vehicles.c
+++ src/vehicles.c
@@ -1,5 +1,36 @@
-#include "../headers/vehicles.h"
+ #include "../headers/vehicles.h"
 
+
+
+/**
+*@func: drawTank
+*
+*@description: Helps me a draw a tank. The trank looks like a Soviet Army tank from the 
+*		1940s. It has the capacity to fire artillery, Has the Red Army logo on it
+*		and can also fire missiles at the designated target. Look forward to work
+*		hard on this one.
+*
+*@params:	double x: Indicates the forward/backward translation units of the vehcile
+*		double y: Indicates the left/right translation unit of the vehicle
+*		double z: Indicates the up/ down translation unit of the vehicle
+*		double th: Indicates the amount by which you want the vehicle to turn
+*
+*@return:	int for now, later might change it to void
+*
+*
+*@todo:		All the features are yet to be added. Just started working on the project
+*/
+
+
+int drawTank(double x,double y,double z,double delta_x,double delta_y,double delta_z, double th)
+{
+
+
+
+
+
+
+}
 
 
 
