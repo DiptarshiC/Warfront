fac3a5e9b0fa3fa585a7bb69beb23ae062b5805a
DiptarshiC <dich4615@colorado.edu>
2019-11-09 16:54:43 -0700

    Added the cone functionalities

--- headers/basic_shapes.h
+++ headers/basic_shapes.h
@@ -3,10 +3,11 @@
 
 
 
+void Vertex(double theta,double alpha);
 void drawCube(double x,double y,double z,double delta_x,double delta_y,double delta_z, double th);
 void drawCylinder(GLfloat radius, GLfloat height, char R,char G char B);
 void drawDome(double x, double y, double z, double delta_x, double delta_y, double delta_z,double th);
-void drawSphere(double x, double y, double z, double delta_x, double delta_y, double delta_z)
-
+void drawSphere(double x, double y, double z, double delta_x, double delta_y, double delta_z);
+void drawCone(double x, double y, double z, double delta_x, double delta_y, double delta_z,double theta);
 
 #endif /*__BASIC_SHAPES_H__*/
