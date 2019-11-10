4b886ed2cade07719275126cba17e5f8ca98d806
DiptarshiC <dich4615@colorado.edu>
2019-11-10 15:07:14 -0700

    Added more details to the buildings

--- src/buildings.c
+++ src/buildings.c
@@ -31,6 +31,13 @@ void drawBuilding(double x, double y, double z, double delta_x, double delta_y, 
         glScaled(delta_x,delta_y,delta_z);
         drawDome(0,0,7,4,4,4,90);
 	drawCube(0,0,0,9,5,7.3,0);
+
+	//Towers at all four corners
+	drawCube(+18,+10,0,9,5,7.3,0);
+	drawCube(-18,+10,0,9,5,7.3,0);
+	drawCube(+18,-10,0,9,5,7.3,0);
+	drawCube(-18,-10,0,9,5,7.3,0);
+
 	glPopMatrix();
 
 }
