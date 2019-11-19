4884a0cdf70482610229da552ade1deaa5916182
DiptarshiC <dich4615@colorado.edu>
2019-11-19 11:09:55 -0700

    Fixed the declaration in the header file

--- headers/basic_shapes.h
+++ headers/basic_shapes.h
@@ -8,7 +8,7 @@ void drawCube(double x,double y,double z,double delta_x,double delta_y,double de
 void drawCylinder(double radius, double height,double x1, double y1, double z1,double delta_x,double delta_y,double delta_z,double th,GLubyte R, GLubyte G,GLubyte B,double alpha );
 void drawDome(double x, double y, double z, double delta_x, double delta_y, double delta_z,double th);
 void drawSphere(double x, double y, double z, double delta_x, double delta_y, double delta_z);
-void drawCone(double radius,double height,double x1, double y1, double z1,double delta_x,double delta_y,double delta_z,double th,GLubyte R,GLubyte G,GLubyte B,double alpha)
+void drawCone(double radius,double height,double x1, double y1, double z1,double delta_x,double delta_y,double delta_z,double th,GLubyte R,GLubyte G,GLubyte B,double alpha);
 void drawCannon(double x, double y, double z, double delta_x, double delta_y, double delta_z);
 void drawTurret(double radius, double height,double x1, double y1, double z1,double delta_x,double delta_y,double delta_z,double th,GLubyte R, GLubyte G,GLubyte B,double alpha,double FireBallRad);
 
