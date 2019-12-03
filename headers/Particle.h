2387d42f0ae4d9dcf3e94d97df416991c7790493
DiptarshiC <dich4615@colorado.edu>
2019-12-03 16:49:06 -0700

    Adding the function for simulatin g event driven mechanisms

--- headers/Particle.h
+++ headers/Particle.h
@@ -2,8 +2,13 @@
 #define __PARTICLE_H__
 
 
+typedef struct Particle_t{
 
-
+	double pos[3];
+	double speed[3];
+	float size, angle, weight;
+	float life;
+}Particle;
 
 
 
---
+++ src/Particle.c
@@ -0,0 +1,6 @@
+#include " ../headers/Particle.h "
+
+
+
+
+
