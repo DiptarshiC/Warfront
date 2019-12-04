5f62ea30f4bcbb580744428cbd0ecee8fb59ebc9
DiptarshiC <dich4615@colorado.edu>
2019-12-04 14:07:58 -0700

    Added the features that would allow me to instatiate multiple objects

--- headers/vehicles.h
+++ headers/vehicles.h
@@ -9,4 +9,41 @@ void Plane(double x, double y, double z, double delta_x, double delta_y, double 
 void initializAsteroids( int ast_pos[][3]);
 void drawASteroid(int ast_pos[][3]);
 
+
+typedef struct AIRPLANE_T
+{
+
+	double x;
+	double y;
+	double z;
+	double delta_x;
+	double delta_y;
+	double delta_z;
+	double horizontal_tilt;
+	double vertical_tilt;
+	double lateral;
+	double blast_rad;
+	int textures[];
+
+}AIRPLANE;
+
+typedef struct TANK_T
+{
+
+        double x;
+        double y;
+        double z;
+        double delta_x;
+        double delta_y;
+        double delta_z;
+        double horizontal_tilt;
+        double vertical_tilt;
+        double lateral;
+        double blast_rad;
+        int textures[];
+
+}TANK;
+
+
+
 #endif /*__VEHICLES_H__*/
